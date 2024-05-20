4054ff0774a2e0b64832cd789379925b165d1e77
lucki wahyu m <luckiwahyum0@gmail.com>
2024-05-20 16:36:50 +0700

    up

--- resources/views/pages/obatpasien.blade.php
+++ resources/views/pages/obatpasien.blade.php
@@ -5,7 +5,7 @@
 
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Data Resep Obat</h1>
+        <h1 class="h3 mb-0 text-gray-800">Data Obat Pasien</h1>
     </div>
 
     <!-- Content Row -->
